d4413d322161ff2a275bd690879fbdb17a1737db
Filip Stangricki <stangricki@gmail.com>
2018-01-22 23:15:25 +0100

    Initial commit

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+	'GOOGLE_CLIENT_ID': '318979260647-7pedne18flt9uj83uim3ho8v2mqfkmfm.apps.googleusercontent.com',
+	'GOOGLE_CLIENT_SECRET': 'ZOoVasGSp27fTXIK_JRmnY2a',
+	'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
+
